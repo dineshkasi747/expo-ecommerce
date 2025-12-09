b6b6741acd12c4573e311258219c406df2aa132c
dineshkasi747 <dineshkasi000@gmail.com>
2025-12-09 17:59:47 +0530

    shop-screen

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,12 +1,62 @@
 import { Stack } from "expo-router";
 import "../global.css"
-import {QueryClient,QueryClientProvider,} from '@tanstack/react-query'
+import {MutationCache, QueryCache, QueryClient,QueryClientProvider,} from '@tanstack/react-query'
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import * as Sentry from '@sentry/react-native';
 
-const queryClient = new QueryClient()
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://3a4b4be1cbf696badb3181f4bead5fd1@o4510228015480832.ingest.us.sentry.io/4510504507998208',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error: any, query) => {
+      Sentry.captureException(error, {
+        tags: {
+          type: "react-query-error",
+          queryKey: query.queryKey[0]?.toString() || "unknown",
+        },
+        extra: {
+          errorMessage: error.message,
+          statusCode: error.response?.status,
+          queryKey: query.queryKey,
+        },
+      });
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error: any) => {
+      // global error handler for all mutations
+      Sentry.captureException(error, {
+        tags: { type: "react-query-mutation-error" },
+        extra: {
+          errorMessage: error.message,
+          statusCode: error.response?.status,
+        },
+      });
+    },
+  }),
+});
+
+
+export default Sentry.wrap(function RootLayout() {
 
   return (
     <ClerkProvider tokenCache={tokenCache}>
@@ -15,5 +65,6 @@ export default function RootLayout() {
       </QueryClientProvider>
     </ClerkProvider>
   );
+
   
-}
+});
