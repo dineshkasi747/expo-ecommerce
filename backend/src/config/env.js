7dd493abe42f936eacb52b0adf16fc563308bca3
dineshkasi747 <dineshkasi000@gmail.com>
2025-12-17 14:34:26 +0530

    dotenv-changed

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,21 +1,31 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export const ENV ={
-    NODE_ENV:process.env.NODE_ENV,
-    PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL,
-    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
-    CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET,
-    CLOUDINARY_CLOUD_NAME:process.env.CLOUDINARY_CLOUD_NAME,
-    INNGEST_SIGNING_KEY:process.env.INNGEST_SIGNING_KEY,
-    ADMIN_EMAIL:process.env.ADMIN_EMAIL,
-    CLIENT_URL:process.env.CLIENT_URL,
-    STRIPE_PUBLISHABLE_KEY:process.env.STRIPE_PUBLISHABLE_KEY,
-    STRIPE_SECRET_KEY:process.env.STRIPE_SECRET_KEY,
-    STRIPE_WEBHOOK_SECRET:process.env.STRIPE_WEBHOOK_SECRET
+dotenv.config({
+  path: path.resolve(__dirname, "../../../.env"),
+});
 
-}
+export const ENV = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 5000,
+
+  DB_URL: process.env.DB_URL,
+
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+  CLIENT_URL: process.env.CLIENT_URL,
+
+  STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+};
