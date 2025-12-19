cefe0e4dd5eb9e3f4f9e5d15938bed3670421868
dineshkasi747 <dineshkasi000@gmail.com>
2025-12-19 11:13:23 +0530

    protect change

--- backend/src/routes/product.router.js
+++ backend/src/routes/product.router.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getAllProducts } from "../controllers/admin.controller.js";
 import { getProductById, getProducts } from "../controllers/product.controller.js";
 
 const router = Router();
 
-router.get("/", protectRoute, getAllProducts);
-router.get("/",getProducts);
+router.get("/",protectRoute,getProducts);
 router.get("/:id", protectRoute, getProductById);
 
 
